f273a50853c246fbd4cc3b2fd7d3d0e7ab0af410
fabriciogs <fabriciogs@gmail.com>
2015-06-24 18:03:42 -0300

    Business + Console app

---
+++ ProjectEuler/ProjectEuler.UI.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectEuler.UI.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+            var answer = new Business.Problem11().Solve();
+            var stopTime = DateTime.Now;
+            var duration = stopTime - startTime;
+            Console.WriteLine("The greatest product of 4 entries, is {0}", answer);
+            Console.WriteLine("Solution took {0} ms", duration.TotalMilliseconds);
+            Console.WriteLine("=> Please, press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
